e5a79dbb618c220f78ba8ddeaa29367a8611ed6b
jpra2 <jprandrade2@gmail.com>
2023-03-23 12:14:08 -0300

    update

--- examples/partition_2d_unstructured.py
+++ examples/partition_2d_unstructured.py
@@ -2,9 +2,7 @@ import os
 from datamanager.meshmanager import CreateMeshProperties, MeshProperty
 from datamanager.mesh_data import MeshData
 from definitions import defpaths
-import metis
 import numpy as np
-import networkx as nx
 import pandas as pd
 
 
@@ -31,11 +29,7 @@ def load_mesh_properties(mesh_name):
     mesh_properties.load_data()
     return mesh_properties
 
-
-# mesh_properties = create_initial_mesh_properties(mesh_path, mesh_name)
-mesh_properties = load_mesh_properties(mesh_name)
-
-def create_partition():
+def create_partition_dep0():
     A = nx.Graph()
     all_adjs = mesh_properties.faces_adj_by_edges
     adjs = all_adjs[(all_adjs[:,0] != -1) & (all_adjs[:,1] != -1)]
@@ -48,16 +42,19 @@ def create_partition():
     gids = np.array(parts)
     return gids
 
-def create_partition2():
+def create_partition_kmeans(x_centroids, y_centroids):
     # Loading of data set into 'cluster'.
     # cluster = pd.read_csv('k-means clustering.csv')
     
+    kmeans_tol = 9e-5
+    k = 20 # Number of clusters
+    number_of_iterations = 100
+
     cluster = pd.DataFrame({
-        'x': mesh_properties.faces_centroids[:,0],
-        'y': mesh_properties.faces_centroids[:, 1]
+        'x': x_centroids,
+        'y': y_centroids
     })
 
-    k = 20 # Number of clusters
     rows = cluster.shape[0] # 'rows' contains the total number of rows in cluster data.
     cols = cluster.shape[1] # 'cols' contains the total number of columns in cluster data.
     
@@ -66,10 +63,12 @@ def create_partition2():
     centroids.set_index('new',inplace = True) # New indices 1 to k are set for the dataframe 'centroids'.
     d = np.random.rand(rows) # Initialization of d which would contain the centroid number closest to data point.
 
-    number_of_iterations = 100
     epsilon = list(range(number_of_iterations)) # 'epsilon' is the sum of squares of distances between points and centroid of a cluster for each iteration
+    epsilon_0 = 0
+    i = 0
+    stop = False
 
-    for i in range(0,number_of_iterations): # This 'for' loop is for iterations.
+    while (i <= number_of_iterations) and (stop == False): # This 'for' loop is for iterations.
 
         for j in range(0,rows): # This 'for' loop finds the centroid number closest to the data point.
             d[j] = ((centroids - cluster.loc[j])**2).sum(axis = 1).idxmin()
@@ -92,21 +91,54 @@ def create_partition2():
         for p in range(0,k): # This 'for' loop calculates square of distances between data points and centroid of each cluster.
             z[p] = ((cluster[cluster['centroid number'] == p+1][['x','y']] - centroids.iloc[p])**2).values.sum()
         epsilon[i] = sum(z) # 'epsilon' is sum of squares of distances between points and centroid of a cluster for each iteration
-    
-    gids = cluster['centroid number'].to_numpy().astype(int)
+        test = abs(epsilon_0 - epsilon[i])
+        print(test)
+        if test < kmeans_tol:
+            stop = True
+
+        epsilon_0 = epsilon[i]
+        
+    import pdb; pdb.set_trace()
+    gids = cluster['centroid number'].to_numpy().astype(int) - 1
     return gids
 
+def create_partition(level, x_centroids, y_centroids, faces_adjacencies):
+    gid_name = 'gid_' + str(level)
+    faces_adjacencies_coarse_name = 'faces_adjacencies_' + str(level)
+    gids = create_partition_kmeans(x_centroids, y_centroids)
+
+    # mesh_data = MeshData(mesh_path=mesh_path)
+    # mesh_data.create_tag(gid_name, data_type='int')
+    # mesh_data.insert_tag_data(gid_name, gids, elements_type='faces', elements_array=mesh_properties.faces)
+    # mesh_data.export_all_elements_type_to_vtk(export_name='test_gids', element_type='faces')
+
+    def create_faces_adjacencies(gids, faces_adjacencies):
+        pass
+        
+
+    mesh_properties.insert_data({
+        gid_name: gids
+    })
+
+    mesh_properties.export_data()
+
+def create_dual(level, x_centroids, y_centroids, adjacencies, gid_level):
+    dual_name = 'dual_' + str(level)
+    gids = np.arange(len(x_centroids))
 
 
 
-gids = create_partition2()
-mesh_data = MeshData(mesh_path=mesh_path)
-mesh_data.create_tag('gid_1', data_type='int')
-mesh_data.insert_tag_data('gid_1', gids, elements_type='faces', elements_array=mesh_properties.faces)
-mesh_data.export_all_elements_type_to_vtk(export_name='test_gids', element_type='faces')
 
+    def create_vertices():
+        pass
 
+    def create_edges():
+        pass
 
+mesh_properties = create_initial_mesh_properties(mesh_path, mesh_name)
 
+create_partition(1, mesh_properties.faces_centroids[:,0], mesh_properties.faces_centroids[:,1], mesh_properties.faces_adj_by_edges)
+
+# mesh_properties = load_mesh_properties(mesh_name)
 
 
