de4bda2758120336e9b51001ca4a6ae6ed3e7d16
jpra2 <jprandrade2@gmail.com>
2023-03-29 11:43:39 -0300

    update

--- mpfa/lsds/gls_weight_2d.py
+++ mpfa/lsds/gls_weight_2d.py
@@ -1,4 +1,5 @@
 from datamanager.arraydatamanager import SuperArrayManager
+import numpy as np
 
 class Weight(SuperArrayManager):
     pass
@@ -39,11 +40,16 @@ class CalculateGlsWeight2D:
 
         for node in self.nodes[self.bool_internal_nodes]:
             nodes_adj = self.nodes_of_nodes[node]
+            n_m1 = len(nodes_adj)
+            m1_local = np.zeros((n_m1, 2*n_m1))            
             edges_adj = self.edges_of_nodes[node]
             centroid_node = self.nodes_centroids[node]
             centroids_nodes_adj = self.nodes_centroids[nodes_adj]
             faces_adj = self.faces_of_nodes[node]
-            dists_nodes = centroids_nodes_adj - centroid_node
+            v_dists_nodes = centroids_nodes_adj - centroid_node
+            for i in range(n_m1):
+                m1_local[i,2*i:2*i+2] = v_dists_nodes[i]
+            
             import pdb; pdb.set_trace()
 
 
